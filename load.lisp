e507b797dd2a826c44ae683596d640e5882eb110
lulook <lulookgamedev@gmail.com>
2025-02-14 16:34:06 +0300

    reading bone data

--- load.lisp
+++ load.lisp
@@ -2,14 +2,28 @@
 
 (defun load-model-data (file)
   (labels (
-      (make-buffer (b) (apply #'concatenate 'vector (coerce b 'list)))
+      (load-weights (ws)
+        (loop for w across ws collect
+          (list (ai:id w) (ai:weight w))
+        )
+      )
+      (load-bones (bs)
+        (when bs
+          (loop for b across bs collect
+            (make-hash
+              :name (ai:name b)
+              :weights (-> b ai:weights load-weights)
+            )
+          )
+        )
+      )
       (load-submesh (m)
         (make-hash
-          :verts (-> m ai:vertices make-buffer)
-          :normals (-> m ai:normals make-buffer)
-          :uvs (-> m ai:texture-coords make-buffer)
-          :indices (-> m ai:faces make-buffer)
-          :bones (-> m ai:bones)
+          :verts (ai:vertices m)
+          :normals (ai:normals m)
+          :uvs (ai:texture-coords m)
+          :indices (ai:faces m)
+          :bones (-> m ai:bones load-bones)
         )
       )
     )
