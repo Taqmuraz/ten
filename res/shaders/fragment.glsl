8a5e7284a7030b11aaf4dad55ba0c3d44cb988b0
lulook <lulookgamedev@gmail.com>
2025-02-18 16:11:44 +0300

    loading color

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -4,8 +4,9 @@ in vec3 worldNormal;
 out vec4 out_Color;
 
 uniform sampler2D texture2d;
+uniform vec4 color;
 
 void main (void)
 {	
-	out_Color = texture(texture2d, uv);
+	out_Color = texture(texture2d, uv) * color;
 }
