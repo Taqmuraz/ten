c11b978b33965f45b57737755aa9d0e922ea811a
lulook <lulookgamedev@gmail.com>
2025-03-24 19:44:15 +0300

    more tests

--- run-perf-test.lisp
+++ run-perf-test.lisp
@@ -3,10 +3,21 @@
     ss (loop for i from 0 below 20 collect (sphere-shape 1 (vector i 10 0)))
     as (append ss ms)
     dt 1/50
-    cycles 100
+    st (shapes-tree as)
   )
-  (format t "Testing ~A cycles with process-forces :~%" cycles)
-  (time (loop repeat cycles do (-> as (process-forces dt) shapes-tree (shapes-tree-sim dt))))
-  (format t "Testing ~A cycles without process-forces :~%" cycles)
-  (time (loop repeat cycles do (-> as shapes-tree (shapes-tree-sim dt))))
+  (macrolet (
+      (perf (cycles tag &body body) `(progn
+          (sb-ext:gc :full t)
+          (format t "Testing ~A for ~A cycles~%" ,tag ,cycles)
+          (time (loop repeat ,cycles do ,@body))
+        )
+      )
+    )
+    (perf 100 "process-forces + shapes-tree + shapes-tree-sim" (-> as (process-forces dt) shapes-tree (shapes-tree-sim dt)))
+    (perf 100 "shapes-tree + shapes-tree-sim" (-> as shapes-tree (shapes-tree-sim dt)))
+    (perf 100 "shapes-tree" (shapes-tree as))
+    (perf 100 "process-forces" (process-forces as dt))
+    (perf 100 "shapes-tree-sim" (shapes-tree-sim st dt))
+    (perf 100 "shapes-tree-contacts" (shapes-tree-contacts st))
+  )
 )
