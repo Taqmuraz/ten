823706e1c45ef5167ed155089f7552a1beb5dbc9
lulook <lulookgamedev@gmail.com>
2025-03-25 11:20:37 +0300

    performance test edits + tuning

--- run-perf-test.lisp
+++ run-perf-test.lisp
@@ -1,29 +1,42 @@
-(lets (
-    ms (-> "res/castle/castle.fbx" load-model-data model-mesh-shapes)
-    ss (loop for i from 0 below 20 collect (sphere-shape 1/4 (vector i 10 0)))
-    as (append ss ms)
-    dt 1/50
-    st (shapes-tree as)
-    c 200
-  )
-  (macrolet (
-      (perf (cycles tag &body body)
-        `(progn
-          (sb-ext:gc :full t)
-          (lets (
-              s (get-time)
-            )
-            (loop repeat ,cycles do ,@body)
-            (format t "Finished ~A for ~A cycles, took ~,1f sec~%" ,tag ,cycles (- (get-time) s))
+(macrolet (
+    (perf (tag &body body)
+      `(progn
+        (sb-ext:gc :full t)
+        (lets (
+            s (get-time)
+            r (progn ,@body)
           )
+          (format t "Finished ~A, took ~,1f sec~%" ,tag (- (get-time) s))
+          r
         )
       )
     )
-    (perf c "process-forces" (process-forces as dt))
-    (perf c "shapes-tree" (shapes-tree as))
-    (perf c "shapes-tree-contacts" (shapes-tree-contacts st))
-    (perf c "shapes-tree-sim" (shapes-tree-sim st dt))
-    (perf c "shapes-tree + shapes-tree-sim" (-> as shapes-tree (shapes-tree-sim dt)))
-    (perf c "process-forces + shapes-tree + shapes-tree-sim" (-> as (process-forces dt) shapes-tree (shapes-tree-sim dt)))
+    (perfc (cycles tag &body body)
+      `(progn
+        (sb-ext:gc :full t)
+        (lets (
+            s (get-time)
+          )
+          (loop repeat ,cycles do ,@body)
+          (format t "Finished ~A for ~A cycles, took ~,1f sec~%" ,tag ,cycles (- (get-time) s))
+        )
+      )
+    )
+  )
+  (format t "~%")
+  (lets (
+      ms (perf "load-model-data + model-mesh-shapes" (-> "res/castle/castle.fbx" load-model-data model-mesh-shapes))
+      ss (perf "sphere-shape x20" (loop for i from 0 below 20 collect (sphere-shape 1/4 (vector i 10 0))))
+      as (append ss ms)
+      dt 1/50
+      st (shapes-tree as)
+      c 200
+    )
+    (perfc c "process-forces" (process-forces as dt))
+    (perfc c "shapes-tree" (shapes-tree as))
+    (perfc c "shapes-tree-contacts" (shapes-tree-contacts st))
+    (perfc c "shapes-tree-sim" (shapes-tree-sim st dt))
+    (perfc c "shapes-tree + shapes-tree-sim" (-> as shapes-tree (shapes-tree-sim dt)))
+    (perfc c "process-forces + shapes-tree + shapes-tree-sim" (-> as (process-forces dt) shapes-tree (shapes-tree-sim dt)))
   )
 )
