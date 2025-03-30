edfdc8ca00a0a90900a90d9a7647286b953a6b75
lulook <lulookgamedev@gmail.com>
2025-03-30 16:36:42 +0300

    physics parralelism experiments

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 ;;;; package.lisp
 
 (defpackage #:ten
-  (:use #:cl #:taclib #:eager-future2)
+  (:use #:cl #:taclib #:eager-future2 #:atomics)
   (:export #:start-demo-game)
 )
--- phys.lisp
+++ phys.lisp
@@ -585,33 +585,61 @@
 )
 
 (defun shapes-tree-contacts (shapes-tree)
-  (lets (r nil c (hash))
+  (lets (memory (atomic (hash)))
     (with-map-keys (shapes tree) shapes-tree
       (labels (
           (walk-tree (node)
             (with-map-keys ((ids :shapes) children) node
-              (loop for (id1 id2) in (all-possible-pairs ids)
+              (remove-if #'null (apply #'concatenate 'list (loop
+                with r = nil
+                for (id1 id2) in (all-possible-pairs ids)
                 for p = (list id1 id2)
-                do (hash-once p c
-                  (with-map-keys ((s1 id1) (s2 id2)) shapes
+                do (swap-atomic-value mem memory
+                  (hash-once p mem
+                    (with-map-keys ((s1 id1) (s2 id2)) shapes
+                      (lets (
+                          cs (shapes-contacts s1 s2)
+                        )
+                        (when cs (setf r
+                          (append r (mapcar (sfun c with-vals c :id-a id1 :id-b id2) cs)))
+                        )
+                      )
+                    )
+                  )
+                  mem
+                )
+                finally (return r)
+              ) (mapcar #'walk-tree children)))
+            )
+          )
+        )
+        (with-map-keys (children) tree
+          (if nil; children
+            (lets (
+                l (length children)
+                ctr (atomic 0)
+                result (atomic nil)
+              )
+              (loop for (a b c d) on children by #'cddddr collect
+                (bt:make-thread
+                  (lambda ()
                     (lets (
-                        cs (shapes-contacts s1 s2)
+                        cs (append (walk-tree a) (walk-tree b) (walk-tree c) (walk-tree d))
                       )
-                      (when cs (setf r
-                        (append r (mapcar (sfun c with-vals c :id-a id1 :id-b id2) cs)))
-                      )
+                      (swap-atomic-value r result (append r cs))
+                      (swap-atomic-value c ctr (+ c 4))
                     )
                   )
                 )
               )
-              (mapc #'walk-tree children)
+              (do () ((>= (atomic-value ctr) l)))
+              (atomic-value result)
             )
+            (walk-tree tree)
           )
         )
-        (walk-tree tree)
       )
     )
-    r
   )
 )
 
